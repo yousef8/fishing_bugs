5ab3fb9a22d453fd0f6eb009751a1ca67a4a888f
Yousef Elsayed <yousef4511@outlook.com>
2024-04-25 13:17:31 +0200

    Fix: Fix syntax error

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'home#indix'
+  root 'home#index'
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
