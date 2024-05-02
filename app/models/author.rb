13e4b3df63d5935cba7c695cf6b31c31576ee9cb
Yousef Elsayed <yousef4511@outlook.com>
2024-05-02 15:18:10 +0300

    Fix association names between comment and author

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,3 @@
 class Author < ApplicationRecord
-  has_many :comment
+  has_many :comments, foreign_key: 'writer_id'
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,3 @@
 class Comment < ApplicationRecord
-  belongs_to :author
+  belongs_to :writer, class_name: 'Author'
 end
